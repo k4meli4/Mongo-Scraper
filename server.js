87506755bcd14b4ed3901c11830df9219d5c2277
k4meli4 <kgracheva@gmail.com>
2018-09-28 15:12:44 -0400

    some bugs need fixing

---
+++ server.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+var router = express.Router();
+
+require("./config/routes")(router);
+
+app.use(express.static(__dirname + "/public"));
+
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(router);
+
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+// Connect to the Mongo DB
+mongoose.Promise = Promise;
+// mongoose.connect(MONGODB_URI);
+
+
+mongoose.connect(MONGODB_URI, function(error){
+    if (error){
+        console.log(error);
+    }
+    else {
+        console.log("mongoose connection is successful");
+    }
+});
+
+app.listen(PORT, function(){
+    console.log("Listening on port:" + PORT);
+});
